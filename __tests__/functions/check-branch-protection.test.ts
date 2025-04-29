0a558923892c2534d94173d002e7e7a6bfff96f0
Matt Mencel <matt@techminer.net>
2025-04-28 22:17:13 -0500

    chore: update test

--- __tests__/functions/check-branch-protection.test.ts
+++ __tests__/functions/check-branch-protection.test.ts
@@ -19,92 +19,92 @@ describe('Check Branch Protection Function', () => {
   })
 
   it('removes branches that do not allow deletions via legacy protection', async () => {
-    github.rest.repos.getBranchProtection = jest.fn()
-      .mockResolvedValueOnce({ data: { allow_deletions: { enabled: false } } })
-      .mockResolvedValueOnce({ data: { allow_deletions: { enabled: true } } }) as unknown as typeof github.rest.repos.getBranchProtection
-    
-    github.rest.repos.getBranchRules = jest.fn()
-      .mockResolvedValueOnce({ data: [] })
-      .mockResolvedValueOnce({ data: [] }) as unknown as typeof github.rest.repos.getBranchRules
+    github.rest.repos.getBranchProtection = jest
+      .fn()
+      .mockResolvedValueOnce({data: {allow_deletions: {enabled: false}}})
+      .mockResolvedValueOnce({data: {allow_deletions: {enabled: true}}}) as unknown as typeof github.rest.repos.getBranchProtection
+
+    github.rest.repos.getBranchRules = jest.fn().mockResolvedValueOnce({data: []}).mockResolvedValueOnce({data: []}) as unknown as typeof github.rest.repos.getBranchRules
 
     await checkBranchProtection(branches)
 
-    expect(branches).toEqual([{ branchName: 'branch2', commmitSha: '67890' }])
+    expect(branches).toEqual([{branchName: 'branch2', commmitSha: '67890'}])
   })
 
   it('removes branches that do not allow deletions via rulesets', async () => {
-    github.rest.repos.getBranchProtection = jest.fn()
-      .mockResolvedValueOnce({ data: { allow_deletions: { enabled: true } } })
-      .mockResolvedValueOnce({ data: { allow_deletions: { enabled: true } } }) as unknown as typeof github.rest.repos.getBranchProtection
-    
-    github.rest.repos.getBranchRules = jest.fn()
-      .mockResolvedValueOnce({ data: [{ allow_deletions: false }] })
-      .mockResolvedValueOnce({ data: [] }) as unknown as typeof github.rest.repos.getBranchRules
+    github.rest.repos.getBranchProtection = jest
+      .fn()
+      .mockResolvedValueOnce({data: {allow_deletions: {enabled: true}}})
+      .mockResolvedValueOnce({data: {allow_deletions: {enabled: true}}}) as unknown as typeof github.rest.repos.getBranchProtection
+
+    github.rest.repos.getBranchRules = jest
+      .fn()
+      .mockResolvedValueOnce({data: [{allow_deletions: false}]})
+      .mockResolvedValueOnce({data: []}) as unknown as typeof github.rest.repos.getBranchRules
 
     await checkBranchProtection(branches)
 
-    expect(branches).toEqual([{ branchName: 'branch2', commmitSha: '67890' }])
+    expect(branches).toEqual([{branchName: 'branch2', commmitSha: '67890'}])
   })
 
   it('keeps branches that allow deletions in both systems', async () => {
-    github.rest.repos.getBranchProtection = jest.fn()
-      .mockResolvedValueOnce({ data: { allow_deletions: { enabled: true } } })
-      .mockResolvedValueOnce({ data: { allow_deletions: { enabled: true } } }) as unknown as typeof github.rest.repos.getBranchProtection
-    
-    github.rest.repos.getBranchRules = jest.fn()
-      .mockResolvedValueOnce({ data: [] })
-      .mockResolvedValueOnce({ data: [] }) as unknown as typeof github.rest.repos.getBranchRules
+    github.rest.repos.getBranchProtection = jest
+      .fn()
+      .mockResolvedValueOnce({data: {allow_deletions: {enabled: true}}})
+      .mockResolvedValueOnce({data: {allow_deletions: {enabled: true}}}) as unknown as typeof github.rest.repos.getBranchProtection
+
+    github.rest.repos.getBranchRules = jest.fn().mockResolvedValueOnce({data: []}).mockResolvedValueOnce({data: []}) as unknown as typeof github.rest.repos.getBranchRules
 
     await checkBranchProtection(branches)
 
     expect(branches).toEqual([
-      { branchName: 'branch1', commmitSha: '12345' },
-      { branchName: 'branch2', commmitSha: '67890' }
+      {branchName: 'branch1', commmitSha: '12345'},
+      {branchName: 'branch2', commmitSha: '67890'}
     ])
   })
 
   it('handles errors when retrieving branch protection', async () => {
-    github.rest.repos.getBranchProtection = jest.fn()
+    github.rest.repos.getBranchProtection = jest
+      .fn()
       .mockRejectedValueOnce(new Error('Error retrieving branch protection'))
-      .mockResolvedValueOnce({ data: { allow_deletions: { enabled: true } } }) as unknown as typeof github.rest.repos.getBranchProtection
-    
-    github.rest.repos.getBranchRules = jest.fn()
-      .mockResolvedValueOnce({ data: [] })
-      .mockResolvedValueOnce({ data: [] }) as unknown as typeof github.rest.repos.getBranchRules
+      .mockResolvedValueOnce({data: {allow_deletions: {enabled: true}}}) as unknown as typeof github.rest.repos.getBranchProtection
+
+    github.rest.repos.getBranchRules = jest.fn().mockResolvedValueOnce({data: []}).mockResolvedValueOnce({data: []}) as unknown as typeof github.rest.repos.getBranchRules
 
     await checkBranchProtection(branches)
 
     expect(branches).toEqual([
-      { branchName: 'branch1', commmitSha: '12345' },
-      { branchName: 'branch2', commmitSha: '67890' }
+      {branchName: 'branch1', commmitSha: '12345'},
+      {branchName: 'branch2', commmitSha: '67890'}
     ])
   })
 
   it('handles errors when retrieving rulesets', async () => {
-    github.rest.repos.getBranchProtection = jest.fn()
-      .mockResolvedValueOnce({ data: { allow_deletions: { enabled: true } } })
-      .mockResolvedValueOnce({ data: { allow_deletions: { enabled: true } } }) as unknown as typeof github.rest.repos.getBranchProtection
-    
-    github.rest.repos.getBranchRules = jest.fn()
-      .mockRejectedValueOnce(new Error('Error retrieving rulesets'))
-      .mockResolvedValueOnce({ data: [] }) as unknown as typeof github.rest.repos.getBranchRules
+    github.rest.repos.getBranchProtection = jest
+      .fn()
+      .mockResolvedValueOnce({data: {allow_deletions: {enabled: true}}})
+      .mockResolvedValueOnce({data: {allow_deletions: {enabled: true}}}) as unknown as typeof github.rest.repos.getBranchProtection
+
+    github.rest.repos.getBranchRules = jest.fn().mockRejectedValueOnce(new Error('Error retrieving rulesets')).mockResolvedValueOnce({data: []}) as unknown as typeof github.rest.repos.getBranchRules
 
     await checkBranchProtection(branches)
 
     expect(branches).toEqual([
-      { branchName: 'branch1', commmitSha: '12345' },
-      { branchName: 'branch2', commmitSha: '67890' }
+      {branchName: 'branch1', commmitSha: '12345'},
+      {branchName: 'branch2', commmitSha: '67890'}
     ])
   })
 
   it('removes multiple branches that do not allow deletions in either system', async () => {
-    github.rest.repos.getBranchProtection = jest.fn()
-      .mockResolvedValueOnce({ data: { allow_deletions: { enabled: false } } })
-      .mockResolvedValueOnce({ data: { allow_deletions: { enabled: false } } }) as unknown as typeof github.rest.repos.getBranchProtection
-    
-    github.rest.repos.getBranchRules = jest.fn()
-      .mockResolvedValueOnce({ data: [{ allow_deletions: false }] })
-      .mockResolvedValueOnce({ data: [{ allow_deletions: false }] }) as unknown as typeof github.rest.repos.getBranchRules
+    github.rest.repos.getBranchProtection = jest
+      .fn()
+      .mockResolvedValueOnce({data: {allow_deletions: {enabled: false}}})
+      .mockResolvedValueOnce({data: {allow_deletions: {enabled: false}}}) as unknown as typeof github.rest.repos.getBranchProtection
+
+    github.rest.repos.getBranchRules = jest
+      .fn()
+      .mockResolvedValueOnce({data: [{allow_deletions: false}]})
+      .mockResolvedValueOnce({data: [{allow_deletions: false}]}) as unknown as typeof github.rest.repos.getBranchRules
 
     await checkBranchProtection(branches)
 
